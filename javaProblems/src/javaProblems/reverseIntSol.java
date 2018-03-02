1fa12b71c79176bab70817a4f152393134d477e2
Hilary Louie <hilhylouie@gmail.com>
2018-03-01 20:20:19 -0800

    Current problems

---
+++ javaProblems/src/javaProblems/reverseIntSol.java
@@ -0,0 +1,25 @@
+/*
+ * Given a 32-bit signed integer, reverse digits of an integer
+ * Input: 123
+ * Output: 321
+ * 
+ * Input: -123
+ * Output: -321
+ * 
+ * Input: 120;
+ * output: 21
+ * 
+ * Assume: can only hold ints within a 32-bit signed integer range
+ * Assume: Returns 0 when the reversed int overflows
+ */
+
+package javaProblems;
+
+public class reverseIntSol {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
